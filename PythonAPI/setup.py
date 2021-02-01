7237c687bd8a844ff6f3e01961321776d7994bb8
Ann Kennedy <annkennedy05@gmail.com>
2021-01-31 19:07:19 -0600

    reorganizing

--- PythonAPI/setup.py
+++ PythonAPI/setup.py
@@ -1,27 +1,28 @@
 from setuptools import setup, Extension
 import numpy as np
+from Cython.Build import cythonize
 
 # To compile and install locally run "python setup.py build_ext --inplace"
 # To install library to Python site-packages run "python setup.py build_ext install"
 
 ext_modules = [
     Extension(
-        'MARS_pycocotools._mask',
-        sources=['common/maskApi.c', '_mask.pyx'],
-        include_dirs = [np.get_include(), 'common'],
+        'pycocotools._mask',
+        sources=['../common/maskApi.c', 'pycocotools/_mask.pyx'],
+        include_dirs = [np.get_include(), '../common'],
         extra_compile_args=[] # previously: ['-Wno-cpp', '-Wno-unused-function', '-std=c99'],
     )
 ]
 
 setup(
-    name='MARS_pycocotools',
+    name='pycocotools',
     packages=['pycocotools'],
-    package_dir = {'pycocotools': '.'},
+    package_dir = {'pycocotools': 'pycocotools'},
     install_requires=[
         'setuptools>=18.0',
         'cython>=0.27.3',
         'matplotlib>=2.1.0'
     ],
     version='2.0',
-    ext_modules= ext_modules
+    ext_modules= cythonize(ext_modules)
 )
