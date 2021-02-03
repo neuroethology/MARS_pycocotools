99669a0d6ba6df4e2014b081867c856a106b68eb
Ann Kennedy <annkennedy05@gmail.com>
2021-02-02 20:56:30 -0600

    renaming

--- PythonAPI/setup.py
+++ PythonAPI/setup.py
@@ -8,22 +8,22 @@ import numpy as np
 
 ext_modules = [
     Extension(
-        'marseval._mask',
-        sources=['../common/maskApi.c', 'marseval/_mask.pyx'],
+        'MARSeval._mask',
+        sources=['../common/maskApi.c', 'MARSeval/_mask.pyx'],
         include_dirs = [np.get_include(), '../common'],
         extra_compile_args=[] # previously: ['-Wno-cpp', '-Wno-unused-function', '-std=c99'],
     )
 ]
 
 setup(
-    name='marseval',
-    packages=['marseval'],
-    package_dir = {'marseval': 'marseval'},
+    name='MARSeval',
+    packages=['MARSeval'],
+    package_dir = {'MARSeval': 'MARSeval'},
     install_requires=[
         'setuptools>=18.0',
         'cython>=0.27.3',
         'matplotlib>=2.1.0'
     ],
-    version='2.0',
+    version='1.0',
     ext_modules= cythonize(ext_modules)
 )
